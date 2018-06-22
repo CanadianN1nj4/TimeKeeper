8b7fc97b5ccffed411ac8280641f1dd397a52d56
tomo <tsutlovic@gmail.com>
2018-06-22 14:14:57 +0200

    Added all files from the project

--- app/src/main/java/tsutlovic/com/timekeeper/ActionAdapter.java
+++ app/src/main/java/tsutlovic/com/timekeeper/ActionAdapter.java
@@ -1,7 +1,11 @@
 package tsutlovic.com.timekeeper;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
@@ -27,4 +31,16 @@ public class ActionAdapter extends ArrayAdapter<Action>{
 
     //endregion
 
+    //region Views
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //conditional statement to see if the convert view is null and use the inflater
+        if (convertView == null) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            convertView = inflater.inflate(resource, parent, false);
+        }
+
+
+        return convertView;
+    }
+    //endregion
 }
